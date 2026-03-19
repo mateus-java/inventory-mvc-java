9ad8cd97427a014cb6c221b50db92de539d8f876
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2026-03-18 21:58:20 -0300

    first commit

---
+++ src/main/java/com/example/mercado_crud/MercadoCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.mercado_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mercado_crud/MercadoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mercado_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
